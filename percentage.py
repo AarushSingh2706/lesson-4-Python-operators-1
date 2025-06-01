f31542626954f578c61b6222879fe86553049f76
AarushSingh2706 <nisha2008mb0035@gmail.com>
2025-06-01 14:05:25 +0530

    calculating percentage

---
+++ percentage.py
@@ -0,0 +1,8 @@
+print("give the marks for the following: ")
+maths = (int(input("maths : ")))
+science = (int(input("science : ")))
+hindi = (int(input("hindi : ")))
+sst = (int(input("sst : ")))
+sum = maths+science+hindi+sst
+percentage = (sum/400)*100
+print("this is your percentage : ",percentage)
